f9c5455d7c08fd4a0811690a8faf497138db3127
Haedon Kaufman <haka9670@gmail.com>
2022-07-13 17:25:03 -0700

    api

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -6,7 +6,7 @@ import formatReservationDate from "./format-reservation-date";
 import formatReservationTime from "./format-reservation-date";
 
 const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "postgres://bjtoubyp:ZiQ28aCN_UP4sMhnzuU2Pck6Wl4ppOYX@heffalump.db.elephantsql.com/bjtoubyp";
+  process.env.REACT_APP_API_BASE_URL || "https://cryptic-anchorage-66498.herokuapp.com/";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
